4533c0e5f7d158527db8a0b2bad5293fb82cc42d
GaoPengY <g13629207421@163.com>
2020-05-27 21:43:55 +0800

    first commit

---
+++ code/day01/Hello.py
@@ -0,0 +1,13 @@
+# python程序的本质就是一个文本文件
+
+print("Hello Python")
+print("Hello World")
+
+# python解释器执行
+# 终端输入 python Hello.py
+# print函数的作用是将""内的内容输出再终端
+# "#" 后的内容python解释器将忽略 -- 单行注释
+"""
+这是python中的多行注释
+"""
+
---
+++ code/day01/arithmetic_operator.py
@@ -0,0 +1,49 @@
+# python中的算数运算符和变量
+
+# 变量的定义
+# 变量名 = 值
+a = 10
+b = 5
+
+# 基本运算
+# 加法
+print(a + b)
+
+# 减法
+print(a - b)
+
+# 乘法
+print(a * b)
+
+# 除法
+print(a / b)
+
+# 模余
+print(a % b)
+
+# 小案例买东西
+# 去商店买5斤香蕉，每斤5元
+
+# 定义价格
+price = 5
+
+# 称重
+wight = 5
+#计算价格
+total = price * wight
+print(total)
+
+# python中的变量类型
+# 案例  小明的个人信息
+# 姓名： 小明
+# 年龄： 19
+# 性别： 男
+# 身高： 1.76m
+# 体重： 65.3kg
+# python在定义变量时不需要指定数据类型
+name = "小明"
+age = 18
+print
+male = "man"
+height = 1.76
+weight = 65.3
